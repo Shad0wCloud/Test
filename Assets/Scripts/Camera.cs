774b750369ecd0e2cdc190a221e642f03e1c6f16
ShadowCloud <miklking9@gmail.com>
2020-06-30 16:06:39 +0300

    Here goes start of git

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W)) {
+            this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z+1);
+        }if (Input.GetKey(KeyCode.S)) {
+            this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z-1);
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            this.transform.position = new Vector3(this.transform.position.x+1, this.transform.position.y, this.transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            this.transform.position = new Vector3(this.transform.position.x-1, this.transform.position.y, this.transform.position.z);
+        }if (Input.GetKey(KeyCode.Q)) {
+            this.transform.localEulerAngles = new Vector3(this.transform.localEulerAngles.x, this.transform.localEulerAngles.y -1, this.transform.localEulerAngles.z);
+        }
+        if (Input.GetKey(KeyCode.E)) {
+            this.transform.localEulerAngles = new Vector3(this.transform.localEulerAngles.x, this.transform.localEulerAngles.y+1, this.transform.localEulerAngles.z);
+        }
+
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y+1, this.transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.RightControl))
+        {
+            this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y-1, this.transform.position.z);
+        }
+    }
+}
---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public GameObject[] objs;
+    public GameObject cursor;
+    int index = 0;
+    float speed = 0.5f;
+    //Instantiate(objs[1], new Vector3(x, 10, y), Quaternion.identity);
+
+    private void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            cursor.transform.position = new Vector3(cursor.transform.position.x, cursor.transform.position.y, cursor.transform.position.z+speed);
+        }
+        if (Input.GetKey(KeyCode.DownArrow)) {
+            cursor.transform.position = new Vector3(cursor.transform.position.x, cursor.transform.position.y, cursor.transform.position.z-speed);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            cursor.transform.position = new Vector3(cursor.transform.position.x-speed, cursor.transform.position.y, cursor.transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            cursor.transform.position = new Vector3(cursor.transform.position.x+speed, cursor.transform.position.y, cursor.transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Instantiate(objs[index], new Vector3(cursor.transform.position.x, cursor.transform.position.y-10, cursor.transform.position.z), Quaternion.identity);
+        }
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            index = 0;
+        }
+        if (Input.GetKey(KeyCode.Alpha2))
+        {
+            index = 1;
+        }
+    }
+}
